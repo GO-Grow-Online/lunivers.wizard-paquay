d8630e55caa990bb6b5d668fd6550a4fe44eaf13
Grow Online <139146912+GO-Grow-Online@users.noreply.github.com>
2025-03-13 13:04:43 +0100

    tweak

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,9 @@
 jQuery(function($) {
-    interactions();
+    search();
+    file();
+    kn_pannel();
 
-    function interactions() {
+    function search() {
         $('form.search').on('submit', function(e) {
             e.preventDefault();
             
@@ -18,9 +20,56 @@ jQuery(function($) {
             }
         });
 
+    }
+
+    function file() {
         $('#file-input').on('change', function() {
             let fileName = this.files.length > 0 ? this.files[0].name : "Aucun fichier";
             $('#file-name').text(fileName);
+        });
+    }
+
+    function kn_pannel(params) {
+        function animateValue(element, start, end, duration) {
+            $({ value: start }).animate({ value: end }, {
+                duration: duration,
+                step: function(now) {
+                    $(element).text(Math.round(now).toLocaleString());
+                },
+                complete: function() {
+                    $(element).attr('data-value', end);
+                }
+            });
+        }
+    
+        function updateKmValues(value) {
+            let annualKm = parseInt(value.replace(/\s/g, ''), 10);
+            let monthlyKm = Math.round(annualKm / 12);
+            let dailyKm = Math.round(annualKm / 365);
+    
+            let currentAnnualKm = parseInt($('[data-annual-km]').attr('data-annual-km'), 10);
+            let currentMonthlyKm = parseInt($('[data-monthly-km]').attr('data-monthly-km'), 10);
+            let currentDailyKm = parseInt($('[data-daily-km]').attr('data-daily-km'), 10);
+    
+            animateValue('[data-annual-km]', currentAnnualKm, annualKm, 500);
+            animateValue('[data-monthly-km]', currentMonthlyKm, monthlyKm, 500);
+            animateValue('[data-daily-km]', currentDailyKm, dailyKm, 500);
+    
+            $('[data-annual-km]').attr('data-annual-km', annualKm);
+            $('[data-monthly-km]').attr('data-monthly-km', monthlyKm);
+            $('[data-daily-km]').attr('data-daily-km', dailyKm);
+        }
+    
+        $('#km_pannel-plus').click(function() {
+            let currentKm = parseInt($('[data-annual-km]').attr('data-annual-km'), 10);
+            let newKm = currentKm + 5000;
+            updateKmValues(newKm.toString());
+        });
+    
+        $('#km_pannel-minus').click(function() {
+            let currentKm = parseInt($('[data-annual-km]').attr('data-annual-km'), 10);
+            let newKm = Math.max(5000, currentKm - 5000); // Empêcher de descendre en dessous de 5000
+            updateKmValues(newKm.toString());
         });
     }
 });
